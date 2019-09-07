81d5000e46b5597ffc1baceccf65e17a2d83c7df
Sean Ng <seannguyen5696@gmail.com>
2019-09-07 10:43:49 +1000

    change home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ class Home extends Component {
     render() {
         return (
             <div>
-                <h1>This is a samole React app, with Redux setup</h1>
+                <h1>This is a sample React app, with Redux setup</h1>
             </div>
         )
     }
